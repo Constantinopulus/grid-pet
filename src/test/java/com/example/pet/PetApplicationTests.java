e26ca43fda51037bb74f8f2e846e817904238ea7
Konstantin Tseluiko <86289855+Konstantin-Tseluiko@users.noreply.github.com>
2022-07-26 14:58:36 +0300

    amends tp build.gradle and added pmd

--- src/test/java/com/example/pet/PetApplicationTests.java
+++ src/test/java/com/example/pet/PetApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.pet;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
